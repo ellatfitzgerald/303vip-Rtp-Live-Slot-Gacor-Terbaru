6118e755382815efa41eb4fe1b033a61a053180c
ellatfitzgerald <110435661+ellatfitzgerald@users.noreply.github.com>
2022-08-02 11:03:57 +0700

    awd

---
+++ spec/303vip-Rtp-Live-Slot-Gacor-Terbaik-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import 303VipRtpLiveSlotGacorTerbaikView from '../lib/303vip-Rtp-Live-Slot-Gacor-Terbaik-view';
+
+describe('303VipRtpLiveSlotGacorTerbaikView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
--- spec/303vip-Rtp-Live-Slot-Gacor-Terbaru-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import 303VipRtpLiveSlotGacorTerbaruView from '../lib/303vip-Rtp-Live-Slot-Gacor-Terbaru-view';
-
-describe('303VipRtpLiveSlotGacorTerbaruView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
